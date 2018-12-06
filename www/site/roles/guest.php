8157004906a5816f346a7a31c764da29ed6d1d11
Kyle Decker <kylebdecker@gmail.com>
2018-12-06 14:21:29 -0500

    Add login functionality

---
+++ www/site/roles/guest.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+  'name'    => 'Guest',
+  'default' => false,
+  'panel'   => false
+];
---
+++ www/site/templates/project_protected.php
@@ -0,0 +1,63 @@
+<?php snippet('head') ?>
+<?php snippet('header') ?>
+
+<main id="swup" class="page-content transition-fade" aria-label="Content">
+
+  <article class="post wrapper h-entry" itemscope itemtype="http://schema.org/BlogPosting">
+
+    <?php if($site->user()): ?>
+
+      <aside class="project-overview">
+        <h1 class="project-title" itemprop="name headline"><?= $page->title() ?></h1>
+        <div class="project-basics">
+          <div class="project-info">
+            <time class="project-year"><?= $page->year() ?></time>
+            <a class="project-website" href="https://<?= $page->website() ?>"><?= $page->website() ?></a>
+          </div>
+          <div class="project-pagination">
+            <a class="project-arrow prev" href="#"><?php snippet('arrow-l') ?></a>
+            <a class="project-arrow next" href="#"><?php snippet('arrow-r') ?></a>
+          </div>
+        </div>
+        <div class="project-challenge">
+          <strong class="project-overview-label">The challenge</strong>
+          <?= $page->challenge() ?>
+        </div>
+        <div class="project-role">
+          <strong class="project-overview-label">My role</strong>
+          <?= $page->role() ?>
+        </div>
+      </aside>
+
+      <div class="post-content e-content" itemprop="articleBody">
+        <?= $page->text()->kirbytext() ?>
+      </div>
+
+    <?php else: ?>
+
+      <?php if($error): ?>
+        <div class="alert">Wrong password</div>
+      <?php endif ?>
+
+      <form method="post">
+        <div>
+          <label for="username"><?= $page->username()->html() ?></label>
+          <input type="text" id="username" name="username" value="guest">
+        </div>
+        <div>
+          <label for="password"><?= $page->password()->html() ?></label>
+          <input type="password" id="password" name="password">
+        </div>
+        <div>
+          <input type="submit" name="login" value="Decrypt">
+        </div>
+      </form>
+
+    <?php endif ?>
+
+    <a class="u-url" href="{{ page.url | relative_url }}" hidden></a>
+  </article>
+
+</main>
+
+<?php snippet('footer') ?>
