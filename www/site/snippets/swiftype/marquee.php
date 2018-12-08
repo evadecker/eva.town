c4f6975e69dd8f8555b980c41abffc109d4f7376
Kyle Decker <kylebdecker@gmail.com>
2018-12-08 15:13:23 -0500

    Parallax instead of marquee - first stab

--- www/site/snippets/swiftype/marquee.php
+++ www/site/snippets/swiftype/marquee.php
@@ -1,17 +1,33 @@
-<div class="full-width transparent">
-  <div class="marquee">
+<div class="full-width">
+  <div class="empties">
     <?php $emptystates = yaml($page->children()->find('empty-states')->emptyStates()) ?>
+    <?php $i = 1 ?>
     <?php foreach($emptystates as $emptystate): ?>
-      <?php $filename = $emptystate['filename'] ?>
-      <picture class="empty-state">
-        <source data-srcset="<?= $page->find('empty-states')->file($filename.'.webp')->url() ?>" type="image/webp">
-        <source data-srcset="<?= $page->find('empty-states')->image($filename.'.png')->url() ?>" type="image/png">
+      <?php
+        $classname;
+        $speed;
+        $filename = $emptystate['filename'];
+        if($i == 4 || $i == 6 || $i == 9 || $i == 12) {
+          $classname = 'background';
+          $speed = -3;
+          $zIndex = 1;
+        } elseif($i == 1 || $i == 3 || $i == 7 || $i == 10) {
+          $classname = 'midground';
+          $speed = -2;
+          $zIndex = 2;
+        } else {
+          $classname = 'foreground';
+          $speed = 2;
+          $zIndex = 3;
+        }
+        $i++;
+      ?>
+      <picture class="empty-state <?= $classname ?>" data-rellax-speed="<?= $speed ?>" data-rellax-zindex="<?= $zIndex ?>">
+        <source srcset="<?= $page->find('empty-states')->file($filename.'.webp')->url() ?>" type="image/webp">
+        <source srcset="<?= $page->find('empty-states')->image($filename.'.png')->url() ?>" type="image/png">
         <img
-          class="lazyload blur-up"
-          data-expand="600"
-          data-src="<?= $page->find('empty-states')->image($filename.'.png')->url() ?>"
           src="<?= $page->find('empty-states')->image($filename.'.svg')->url() ?>"
-          alt="Blah"
+          alt="<?= $emptystate['alt'] ?>"
         />
       </picture>
     <?php endforeach ?>
