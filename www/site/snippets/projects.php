1d07b3d256fb65da57dae67be55ccf7b1a54d59e
Kyle Decker <kylebdecker@gmail.com>
2018-12-07 21:30:25 -0500

    Fix about logo

--- www/site/snippets/projects.php
+++ www/site/snippets/projects.php
@@ -9,10 +9,9 @@
         <p><?= $project->tagline()->html() ?></p>
       </a>
     <?php endif ?>
-    
   <?php endforeach ?>
   <a class="home-project home-project-about" href="<?= $site->find('about')->url() ?>">
-    <span class="project-logo"></span>
+    <span class="project-logo"><?php snippet('about/logo') ?></span>
     <h3>About <span class="project-arrow"><?php snippet('arrow-r') ?></span></h3>
     <p>Information on Kyle, the human. Contact info, resume, job history, links&hellip; the fun is endless.</p>
   </a>
