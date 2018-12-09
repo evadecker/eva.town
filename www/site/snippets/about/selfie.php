bafafaaf6f9f9d5b1e59947f4ce120a382264b6a
Kyle Decker <kylebdecker@gmail.com>
2018-12-09 16:32:38 -0500

    Copy edits and lazyload tweaks

--- www/site/snippets/about/selfie.php
+++ www/site/snippets/about/selfie.php
@@ -2,12 +2,10 @@
   <figure>
     <!-- 550 / 440 * 100% -->
     <picture class="ratio-box" style="padding-top: 125%"> 
-      <source data-srcset="<?= $page->file('kyle-decker-portrait-sketch.webp')->url() ?>" type="image/webp">
-      <source data-srcset="<?= $page->image('kyle-decker-portrait-sketch.png')->url() ?>" type="image/png"> 
+      <source srcset="<?= $page->file('kyle-decker-portrait-sketch.webp')->url() ?>" type="image/webp">
+      <source srcset="<?= $page->image('kyle-decker-portrait-sketch.png')->url() ?>" type="image/png"> 
       <img
-        class="lazyload fade-in"
-        data-src="<?= $page->image('kyle-decker-portrait-sketch.png')->url() ?>"
-        src="<?= $page->image('kyle-decker-portrait-sketch.svg')->url() ?>"
+        src="<?= $page->image('kyle-decker-portrait-sketch.png')->url() ?>"
         alt="Portrait of the guy who made this site."
       />
     </picture>
