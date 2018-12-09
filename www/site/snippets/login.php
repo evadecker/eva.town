091e9e6286dd52747a8677c072c79957196ef04d
Kyle Decker <kylebdecker@gmail.com>
2018-12-08 19:06:47 -0500

    Add lock icon

--- www/site/snippets/login.php
+++ www/site/snippets/login.php
@@ -5,13 +5,13 @@
     <p>I can’t share this project because of some legal mumbo-jumbo. If you’re a recruiter or potential employer, grab the passphrase by emailing <a href="mailto:kyle@kyledecker.me">kyle@kyledecker.me</a>. Otherwise, please check out my openly available case studies for <a href="/work/swiftype">Swiftype</a> and <a href="/work/wintr-mx">Wintr.mx</a>.</p>
     <form method="post">
       <input type="hidden" id="username" name="username" value="guest">
+      <?php if($error): ?>
+        <div class="error-message">Incorrect passphrase. Please try again. Spaces are characters, too.</div>
+      <?php endif ?>
       <div class="password-submit <?php if($error) echo 'try-again' ?>">
-        <?php if($error): ?>
-          <div class="visually-hidden">Incorrect password. Please try again.</div>
-        <?php endif ?>
-        <input class="password-submit" type="text" id="password" name="password" placeholder="Enter the secret passphrase">
-        <input type="submit" name="login" value="Go">
+        <input class="password-submit" type="text" id="password" name="password" maxlength="48" placeholder="Enter the secret passphrase">
       </div>
+      <input class="visually-hidden" type="submit" name="login" value="Go">
     </form>
   </div>
   <p class="garbled" aria-hidden="true">▘▙▗ ▚▟▖▟▜▂▘ ▙▗▚▂ ▟▗ ▟▜▘▙▗▚▟▟▜▘▜▂▘▙▗▚▘ ▟▟▘ ▜▂▘▙▚▟ ▟▜▘▘ ▙▗▂▚▟ ▟▙▗▚▟▟ ▜▖ ▘ ▙▗▂▙▗▚▟▖▟▙▗ ▚▟▟▜  ▗▚▟▟▖▟▜▘▙ ▟▜ ▘▗▚ ▟▟▜ ▘▜▘ ▙▗▚▂▚▟▟▜▗▘▙▗ ▚▟▟ ▖▜ ▖▘▙▂▗▚ ▟ ▗▟▚▟▟▜ ▚▟ ▟▜▘▙ ▗▚▚▟▘▗▗ ▙▗ ▚▟▂▟▜▘ ▟▜▖▘▙▗▚ ▘▙ ▗▚▟▖▟ ▟▗▜▘▙ ▗▂▚▙▗▚▟▙▘▙ ▗▟▜▘▗▙▗▂ ▚▟ ▟▟▜ ▘▙▟ ▖▟▜▟ ▟▜▘▙ ▗ ▚ ▟ ▟▜▘▙ ▗</p>
