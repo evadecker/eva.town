905c34f27c96c5e0a3438e08e96ffdc2fc0829a5
Kyle Decker <kylebdecker@gmail.com>
2018-12-14 02:28:39 -0500

    Reintroduce marquee - CSS onlyyy

--- www/site/snippets/swiftype/marquee.php
+++ www/site/snippets/swiftype/marquee.php
@@ -1,35 +1,32 @@
-<div class="full-width">
-  <div class="empties">
-    <?php $emptystates = yaml($page->children()->find('empty-states')->emptyStates()) ?>
-    <?php $i = 1 ?>
-    <?php foreach($emptystates as $emptystate): ?>
-      <?php
-        $classname;
-        $speed;
-        $filename = $emptystate['filename'];
-        if($i == 4 || $i == 6 || $i == 9 || $i == 12) {
-          $classname = 'background';
-          $speed = -3;
-          $zIndex = 1;
-        } elseif($i == 1 || $i == 3 || $i == 7 || $i == 10) {
-          $classname = 'midground';
-          $speed = -2;
-          $zIndex = 2;
-        } else {
-          $classname = 'foreground';
-          $speed = 2;
-          $zIndex = 3;
-        }
-        $i++;
-      ?>
-      <picture class="empty-state <?= $classname ?>" data-rellax-speed="<?= $speed ?>" data-rellax-zindex="<?= $zIndex ?>">
-        <source srcset="<?= $page->find('empty-states')->file($filename.'.webp')->url() ?>" type="image/webp">
-        <source srcset="<?= $page->find('empty-states')->image($filename.'.png')->url() ?>" type="image/png">
-        <img
-          src="<?= $page->find('empty-states')->image($filename.'.svg')->url() ?>"
-          alt="<?= $emptystate['alt'] ?>"
-        />
-      </picture>
-    <?php endforeach ?>
+<div class="full-width transparent">
+  <div class="marquee">
+    <?php for($n = 1; $n <= 2; $n++): // Duplicate marquee twice ?>
+      <div class="marquee-half" <?php e($n == 2, 'aria-hidden="true"') ?>>
+        <?php $emptystates = yaml($page->children()->find('empty-states')->emptyStates()) ?>
+        <?php $i = 1 ?>
+        <?php foreach($emptystates as $emptystate): ?>
+          <?php
+            $classname;
+            $filename = $emptystate['filename'];
+            if($i == 4 || $i == 6 || $i == 9 || $i == 12) {
+              $classname = 'background';
+            } elseif($i == 1 || $i == 3 || $i == 7 || $i == 10) {
+              $classname = 'midground';
+            } else {
+              $classname = 'foreground';
+            }
+            $i++;
+          ?>
+          <picture class="empty-state <?= $classname ?>">
+            <source srcset="<?= $page->find('empty-states')->file($filename.'.webp')->url() ?>" type="image/webp">
+            <source srcset="<?= $page->find('empty-states')->image($filename.'.png')->url() ?>" type="image/png">
+            <img
+              src="<?= $page->find('empty-states')->image($filename.'.svg')->url() ?>"
+              alt="<?= $emptystate['alt'] ?>"
+            />
+          </picture>
+        <?php endforeach ?>
+      </div>
+    <?php endfor ?>
   </div>
 </div>
