358688cdc00dcc4b0ac78ca4b383fe36cb30b884
Kyle Decker <kylebdecker@gmail.com>
2018-12-14 01:43:25 -0500

    Fix rellax glitchiness, use fade for quotes

--- www/site/snippets/swiftype/dashboard-annotations.php
+++ www/site/snippets/swiftype/dashboard-annotations.php
@@ -12,17 +12,25 @@
         />
       </picture>
       <div class="annotations">
-        <div class="annotation rellax" style="top: 2%; left: 20%" data-direction="left" data-rellax-speed="-0.5">
-          <div class="annotation-popover">Top-level navigation takes top-level prominence. <em>Engines</em> is enlarged and moved away from <em>Docs</em> and <em>Support</em>.</div>
+        <div class="annotation" style="top: 2%; left: 20%" data-direction="left">
+          <div class="annotation-rellax">
+            <div class="annotation-popover">Top-level navigation takes top-level prominence. <em>Engines</em> is enlarged and moved away from <em>Docs</em> and <em>Support</em>.</div>
+          </div>
         </div>
-        <div class="annotation rellax" style="top: 15%; left: 94%" data-direction="top-right" data-rellax-speed="-0.5">
-          <div class="annotation-popover">Three account menus merge into one, allowing users to manage  settings, switch accounts, and log out from one place.</div>
+        <div class="annotation" style="top: 15%; left: 94%" data-direction="top-right">
+          <div class="annotation-rellax">
+            <div class="annotation-popover">Three account menus merge into one, allowing users to manage  settings, switch accounts, and log out from one place.</div>
+          </div>
         </div>
-        <div class="annotation rellax" style="top: 56%; left: 5.5%" data-direction="top-left" data-rellax-speed="-0.5">
-          <div class="annotation-popover">A persistent sidebar beneath the engines dropdown clarifies navigation. The active page is visible without hovering. The dark background draws focus to the content.</div>
+        <div class="annotation" style="top: 56%; left: 5.5%" data-direction="top-left">
+          <div class="annotation-rellax">
+            <div class="annotation-popover">A persistent sidebar beneath the engines dropdown clarifies navigation. The active page is visible without hovering. The dark background draws focus to the content.</div>
+          </div>
         </div>
-        <div class="annotation rellax" style="top: 62%; left: 79%" data-direction="top" data-rellax-speed="-0.5">
-          <div class="annotation-popover">Fonts are larger and bolder. All-caps text has been eliminated almost everywhere.</div>
+        <div class="annotation" style="top: 62%; left: 79%" data-direction="top">
+          <div class="annotation-rellax">
+            <div class="annotation-popover">Fonts are larger and bolder. All-caps text has been eliminated almost everywhere.</div>
+          </div>
         </div>
       </div>
     </div>
