ccc8b4bbb6e0fa9082b2a38d861a06d9ec6adfd5
Kyle Decker <kylebdecker@gmail.com>
2018-12-15 17:14:16 -0500

    Copy edits

--- www/site/snippets/swiftype/acumin.php
+++ www/site/snippets/swiftype/acumin.php
@@ -3,6 +3,6 @@
     <source src="<?= $page->video('acumin.mp4')->url() ?>" type="video/mp4">
   </video>
   <figcaption>
-  Acumin Pro, designed by Robert Slimbach of <a href="https://www.adobe.com/products/type.html">Adobe</a>. Shown in Light, Regular, Semibold, and Bold.
+  Acumin Pro, designed by Robert Slimbach of <a href="https://www.adobe.com/products/type.html">Adobe</a>.
   </figcaption>
 </figure>
--- www/site/snippets/swiftype/national.php
+++ www/site/snippets/swiftype/national.php
@@ -3,6 +3,6 @@
     <source src="<?= $page->video('national.mp4')->url() ?>" type="video/mp4">
   </video>
   <figcaption>
-  National, designed by Kris Sowersby of <a href="https://klim.co.nz/">Klim Foundry</a>. Shown in Light, Regular, Semibold, and Bold.
+  National, designed by Kris Sowersby of <a href="https://klim.co.nz/">Klim Foundry</a>.
   </figcaption>
 </figure>
