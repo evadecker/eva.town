a9eef6b040a46c4a441fd096e8bcf481cf5a03ce
Kyle Decker <kylebdecker@gmail.com>
2018-12-21 02:50:09 -0500

    Add announcement banner to about page

--- www/site/templates/about.php
+++ www/site/templates/about.php
@@ -3,6 +3,9 @@
 
 <main id="swup" class="page-content transition-fade" aria-label="Content">
   <article class="post wrapper about-page">
+    <div class="announcement">
+      This site just launched! Please email <a href="mailto:kyle@kyledecker.me">kyle@kyledecker.me</a> if you find anything misspelled, misplaced, misguided, or malfunctioning.
+    </div>
     <h1 class="visually-hidden"><?= $page->title() ?></h1>
     <div class="post-content">
       <?= $page->text()->kirbytext() ?>
