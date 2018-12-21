79c7c87605d8fe1a0f4912a9004919039e2f70e7
Kyle Decker <kylebdecker@gmail.com>
2018-12-21 02:14:29 -0500

    Modify login success message, swap font specimen order

--- www/site/controllers/project.php
+++ www/site/controllers/project.php
@@ -6,7 +6,7 @@ return function($site, $pages, $page) {
   if(r::is('post')) {
     if(($user = $site->user('guest')) && $user->login(get('password'))) {
       $response['success'] = true;
-      $response['message'] = "And they control our government at the highest level!!!  🐍🐍🐍<br/><em>Redirecting&hellip;</em>";
+      $response['message'] = "Correct! And they control our government at the highest level! <br/><em>Redirecting&hellip;</em> 🐍🐍🐍";
     } else {
       $response['success'] = false;
       $response['message'] =  "Incorrect passphrase. Try again, and be sure to include spaces.";
--- www/site/snippets/swiftype/national.php
+++ www/site/snippets/swiftype/national.php
@@ -2,14 +2,14 @@
   <div class="font-specimen">
     <picture>
       <img
-        src="<?= $page->image('national-specimen-1.svg')->url() ?>"
-        alt="A font specimen for National, showing its display in all-caps settings."
+        src="<?= $page->image('national-specimen-2.svg')->url() ?>"
+        alt="A font specimen for National, showing its display in paragraphs and headings."
       />
     </picture>
     <picture>
       <img
-        src="<?= $page->image('national-specimen-2.svg')->url() ?>"
-        alt="A font specimen for National, showing its display in paragraphs and headings."
+        src="<?= $page->image('national-specimen-1.svg')->url() ?>"
+        alt="A font specimen for National, showing its display in all-caps settings."
       />
     </picture>
   </div>
