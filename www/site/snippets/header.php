702d7c08e4f7ca6912634d3108f17ca2c9cf22f0
Kyle Decker <kylebdecker@gmail.com>
2018-12-21 02:50:09 -0500

    Add announcement banner to about page

--- www/site/snippets/header.php
+++ www/site/snippets/header.php
@@ -1,5 +1,5 @@
 <header id="header" role="banner">
-	<nav class="wrapper" role="navigation">
+	<nav class="site-nav wrapper" role="navigation">
 		<a class="site-title page-link <?php e($page->isHomePage(), 'active') ?>" rel="author" href="<?= url() ?>"><?php snippet('logo') ?></a>
     <?php foreach($pages->visible() as $p): ?>
       <a class="page-link <?php e($p->isOpen(), 'active') ?>" href="<?= $p->url() ?>"><?php echo $p->title()->html() ?></a>
