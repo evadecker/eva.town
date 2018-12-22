6182857c238dda5793f633fc4847f88a24706473
Kyle Decker <kylebdecker@gmail.com>
2018-12-22 16:27:50 -0500

    Make post callouts more consistent, tweak copy, speed up circle of fifths transition, add EmojiOne credits to Readme

--- www/site/snippets/swiftype/hierarchy.php
+++ www/site/snippets/swiftype/hierarchy.php
@@ -1,17 +1,19 @@
 <div class="post-callout">
   <h3>There are two types of hierarchy:</h3>
-  <ol>
+  <ul class="emoji-list">
     <li>
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f4d6.png">
       <dl>
         <dt>content hierarchy</dt>
         <dd>also known as content architecture, this refers to how content is organized in relation to itself</dd>
       </dl>
     </li>
     <li>
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f441.png">
       <dl>
         <dt>visual hierarchy</dt>
         <dd>the organization of content on-screen; normally mirrors content hierarchy, but not always; positioning, size, weight, color, motion, and context all help establish visual hierarchy</dd>
       </dl>
     </li>
-  </ol>
+  </ul>
 </div>
