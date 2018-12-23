d7ff9ca9d30a4de3e900de741e95ce82e1d3218f
Kyle Decker <kylebdecker@gmail.com>
2018-12-23 01:05:53 -0500

    HTMl5 validation

--- www/site/templates/project.php
+++ www/site/templates/project.php
@@ -6,7 +6,6 @@
   <div class="project-hero">
     <?php
       $page->hasPrevVisible() ? $prev = $page->prevVisible() : $prev = $page->siblings()->visible()->last();
-
       $page->hasNextVisible() ? $next = $page->nextVisible() : $next = $page->siblings()->visible()->first();
     ?>
     <div class="project-pagination">
@@ -15,30 +14,29 @@
     </div>
     <div class="project-info">
       <time class="project-year transition-swipe"><?= $page->year() ?></time>
-      <h1 class="project-title transition-swipe" itemprop="name headline"><?= $page->title() ?></h1>
+      <h1 class="project-title transition-swipe"><?= $page->title() ?></h1>
       <a class="project-website transition-swipe" href="https://<?= $page->website() ?>"><?= $page->website() ?></a>
     </div>
   </div>
 
   <article class="post wrapper h-entry transition-swipe" itemscope itemtype="http://schema.org/BlogPosting">
     <div class="post-content e-content" itemprop="articleBody">
-    <?php if($page->isPasswordProtected() == '1' && !$site->user()): ?>
-      <?php snippet('login') ?>
-    <?php else: ?>
-      <aside class="project-overview mobile-fullwidth">
-        <div class="project-challenge">
-          <strong class="project-overview-label">The challenge</strong>
-          <?= $page->challenge() ?>
-        </div>
-        <div class="project-role">
-          <strong class="project-overview-label">My role</strong>
-          <?= $page->role() ?>
-        </div>
-      </aside>
-      <?= $page->text()->kirbytext() ?>
-    <?php endif ?>
+      <?php if($page->isPasswordProtected() == '1' && !$site->user()): ?>
+        <?php snippet('login') ?>
+      <?php else: ?>
+        <aside class="project-overview mobile-fullwidth">
+          <div class="project-challenge">
+            <strong class="project-overview-label">The challenge</strong>
+            <?= $page->challenge() ?>
+          </div>
+          <div class="project-role">
+            <strong class="project-overview-label">My role</strong>
+            <?= $page->role() ?>
+          </div>
+        </aside>
+        <?= $page->text()->kirbytext() ?>
+      <?php endif ?>
     </div>
-
     <a class="u-url" href="<?= $page->url() ?>" hidden></a>
   </article>
 
