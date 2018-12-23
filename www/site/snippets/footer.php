6a7a5807ed4c2f92ae190b67e177dc677b4d0db8
Kyle Decker <kylebdecker@gmail.com>
2018-12-23 01:38:25 -0500

    Fix accessibility issues

--- www/site/snippets/footer.php
+++ www/site/snippets/footer.php
@@ -1,5 +1,5 @@
     <footer class="site-footer h-card wrapper">
-      <a href="<?= $site->url() ?>" class="logo"><?php snippet('logo') ?></a>
+      <a href="<?= $site->url() ?>" class="logo"><span class="visually-hidden"><?= $site->title() ?></span><?php snippet('logo') ?></a>
       &copy; <?= date("Y") ?>, <?= $site->author() ?>. Please don't steal.
     </footer>
   </body>
