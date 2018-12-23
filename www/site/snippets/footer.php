4a9c157ff365c6175af9a48cf293a7c61861b504
Kyle Decker <kylebdecker@gmail.com>
2018-12-23 03:25:55 -0500

    Combine footer with quotes area

--- www/site/snippets/footer.php
+++ www/site/snippets/footer.php
@@ -1,4 +1,4 @@
-    <footer class="site-footer h-card wrapper">
+    <footer class="site-footer h-card">
       <a href="<?= $site->url() ?>" class="logo"><span class="visually-hidden"><?= $site->title() ?></span><?php snippet('logo') ?></a>
       &copy; <?= date("Y") ?>, <?= $site->author() ?>. Please don't steal.
     </footer>
