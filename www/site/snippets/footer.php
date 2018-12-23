c2bd1bfa295261eec8da5cea5fa0ce228f3d0e34
Kyle Decker <kylebdecker@gmail.com>
2018-12-23 00:47:37 -0500

    Fix PHP error

--- www/site/snippets/footer.php
+++ www/site/snippets/footer.php
@@ -1,6 +1,6 @@
     <footer class="site-footer h-card wrapper">
       <a href="<?= $site->url() ?>" class="logo"><?php snippet('logo') ?></a>
-      &copy; <?= date(Y) ?>, <?= $site->author() ?>. Please don't steal.
+      &copy; <?= date("Y") ?>, <?= $site->author() ?>. Please don't steal.
     </footer>
   </body>
 </html>
