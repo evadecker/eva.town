90ea0b1527f2e87d5ef3f0544c264493682d109b
Kyle Decker <kylebdecker@gmail.com>
2018-12-23 00:00:32 -0500

    Add social icons

--- www/site/snippets/head.php
+++ www/site/snippets/head.php
@@ -7,20 +7,23 @@
 
     <?php if($page->isHomepage()): ?>
       <title><?= html($page->title()) ?></title>
+      <meta property="og:title" content="<?= html($page->title()) ?>">
       <meta name="description" content="<?= html($site->description()) ?>" />
       <meta property="og:description" content="<?= html($site->description()) ?>" />
     <?php else: ?>
       <title><?= html($page->title()) ?> ・ <?= html($site->title()) ?></title>
+      <meta property="og:title" content="<?= html($page->title()) ?> ・ <?= html($site->title()) ?>">
       <meta name="description" content="<?= html($page->text()->excerpt(500)) ?>" />
       <meta property="og:description" content="<?= html($page->text()->excerpt(500)) ?>" />
     <?php endif ?>
 
-    <meta property="og:title" content="<?= html($page->title()) ?>" />
     <meta property="og:site_name" content="<?= html($site->title()) ?>"/>
     <meta property="og:url" content="<?= html($page->url()) ?>" />
     <meta property="og:type" content="website"/>
-    <meta property="og:image" content="http://kyledecker.me/assets/images/facebook-preview.png"/>
+    <meta property="og:image" content="<?= $site->url() ?>/assets/images/social-preview.png"/>
     <meta name="author" content="<?= $site->author()->html() ?>" />
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@<?= $site->twitter() ?>">
     
     <link rel="icon" type="image/png" href="<?= $site->url() ?>/favicon-96x96.png" sizes="96x96" />
     <link rel="icon" type="image/png" href="<?= $site->url() ?>/favicon-32x32.png" sizes="32x32" />
