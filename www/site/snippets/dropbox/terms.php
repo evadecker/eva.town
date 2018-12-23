aa8aba34badf946e66ef77eea3ecd764bb445e97
Kyle Decker <kylebdecker@gmail.com>
2018-12-23 01:03:55 -0500

    Add alt text for emojis

--- www/site/snippets/dropbox/terms.php
+++ www/site/snippets/dropbox/terms.php
@@ -2,49 +2,49 @@
   <h3>Before we begin, some helpful terms:</h3>
   <ul class="emoji-list">
     <li>
-      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f60e.png">
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f60e.png" alt="Sunglasses emoji">
       <dl>
         <dt>Growth Platform</dt>
         <dd>my team, pictured above; buncha cool, nice nerds</dd>
       </dl>
     </li>
     <li>
-      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f4e3.png">
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f4e3.png" alt="Megaphone emoji">
       <dl>
         <dt>Megaphone</dt>
         <dd>the product suite created by Growth Platform; includes Bluemail, Bluenote, Prompt, and other utilities</dd>
       </dl>
     </li>
     <li>
-      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f4e9.png">
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f4e9.png" alt="Envelope with arrow pointing down emoji">
       <dl>
         <dt>Bluemail</dt>
         <dd>an email creation and delivery system; if you’ve ever gotten an email about file activity in Dropbox, that came from Bluemail</dd>
       </dl>
     </li>
     <li>
-      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f4f2.png">
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f4f2.png" alt="Phone with arrow pointing right emoji">
       <dl>
         <dt>Bluenote</dt>
         <dd>like Bluemail, but for push notifications</dd>
       </dl>
     </li>
     <li>
-      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f4b5.png">
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f4b5.png" alt="Money emoji">
       <dl>
         <dt>Prompt</dt>
         <dd>Dropbox’s ads system; because Dropbox does not display third-party ads, Prompt is composed of upsells, feature announcements, tips, and account messages like letting a user know they are out of space</dd>
       </dl>
     </li>
     <li>
-      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f9d9.png">
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f9d9.png" alt="Wizard emoji">
       <dl>
         <dt>Stormcrow</dt>
         <dd>a system for feature gating; whether a user is "Stormcrowed in" determines whether or not they can see a feature on Dropbox</dd>
       </dl>
     </li>
     <li>
-      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f6e4.png">
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f6e4.png" alt="Train tracks emoji">
       <dl>
         <dt>Journey Builder</dt>
         <dd>originally known as Tracks; the system that ties together Bluemail, Bluenote, and Prompt; it’s what this case study is about</dd>
--- www/site/snippets/swiftype/lessons.php
+++ www/site/snippets/swiftype/lessons.php
@@ -2,19 +2,19 @@
   <h3>Lessons learned:</h3>
   <ul class="emoji-list">
     <li>
-      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f501.png">
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f501.png" alt="Repeat emoji">
       <strong>Process exists for a reason.</strong> If process is solid, it'll speed you up, not slow you down.
     </li>
     <li>
-      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/26aa.png">
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/26aa.png" alt="White circle emoji">
       <strong>What's unique is not always what's useful.</strong> A plain interface font is probably a good interface font. Your brand's voice shouldn't overpower the utility of your app.
     </li>
     <li>
-      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f4ca.png">
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f4ca.png" alt="Bar chart emoji">
       <strong>Taking a stance becomes easier with data.</strong> Numbers reveal invisible truths. Use them to make improvements, but never let data lead you to abuse inborn addictive tendencies.
     </li>
     <li>
-      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f418.png">
+      <img class="emoji" src="<?= $site->url() ?>/assets/emoji/1f418.png" alt="Elephant emoji">
       <strong>Make incremental changes whenever possible.</strong> It sustains team momentum and helps surface issues early. You eat an elephant one bite at a time.
     <li>
   </ul>
