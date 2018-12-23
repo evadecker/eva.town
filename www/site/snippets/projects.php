749b44d2ef51db248987568f33f994652e724f81
Kyle Decker <kylebdecker@gmail.com>
2018-12-23 01:38:25 -0500

    Fix accessibility issues

--- www/site/snippets/projects.php
+++ www/site/snippets/projects.php
@@ -5,20 +5,20 @@
         <span class="project-logo">
           <?php snippet($project->slug().'/logo') ?>
         </span>
-        <h3>
+        <h2>
           <?= $project->title()->html() ?>
           <span class="arrow"><?php snippet('arrow-r') ?></span>
           <?php if ($project->isPasswordProtected() == '1' && !$site->user()): ?>
             <span class="project-locked"><?php snippet('icons/lock') ?></span>
           <?php endif ?>
-        </h3>
+        </h2>
         <p><?= $project->tagline()->html() ?></p>
       </a>
     <?php endif ?>
   <?php endforeach ?>
   <a class="home-project home-project-about" href="<?= $site->find('about')->url() ?>">
     <span class="project-logo"><?php snippet('about/logo') ?></span>
-    <h3>About <span class="arrow"><?php snippet('arrow-r') ?></span></h3>
+    <h2>About <span class="arrow"><?php snippet('arrow-r') ?></span></h2>
     <p>On the most viewed thing I’ve ever created, plus: social links, design principles, contact info.</p>
   </a>
 </div>
