2dd7980ac707b4681e1039bf1519ce4fb1da50d7
Kyle Decker <kylebdecker@gmail.com>
2019-01-03 04:01:28 -0500

    Dropbox first draft complete

--- www/site/snippets/dropbox/peewee.php
+++ www/site/snippets/dropbox/peewee.php
@@ -1,5 +1,5 @@
 <figure>
-  <video poster="<?= $page->image('breakfast-machine.jpg')->url() ?>" class="js-player" playsinline controls>
+  <video poster="<?= $page->image('breakfast-machine.jpg')->url() ?>" class="js-player" playsinline controls preload="none">
     <source src="<?= $page->file('breakfast-machine.mp4')->url() ?>" type="video/mp4">
     <source src="<?= $page->file('breakfast-machine.webm')->url() ?>" type="video/webm">
   </video>
---
+++ www/site/snippets/dropbox/research-video.php
@@ -0,0 +1,6 @@
+<figure>
+  <video poster="<?= $page->image('research-sessions.png')->url() ?>" class="js-player" autoplay loop muted preload="none">
+    <source src="<?= $page->file('research-sessions.mp4')->url() ?>" type="video/mp4">
+    <source src="<?= $page->file('research-sessions.webm')->url() ?>" type="video/webm">
+  </video>
+</figure>
