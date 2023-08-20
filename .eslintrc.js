ab53de8e1fd08a7b584eaa05c5dd5b2ea1f6d83e
Eva Decker <itsevadecker@gmail.com>
2023-08-20 13:43:43 -0400

    Eslint config and fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,44 +6,9 @@ module.exports = {
     },
     extends: [
         "standard-with-typescript",
-        "plugin:react/recommended"
-    ],
-    overrides: [
-        {
-            env: {
-                "node": true
-            },
-            files: [
-                ".eslintrc.{js,cjs}"
-            ],
-            parserOptions: {
-                sourceType: "script"
-            }
-        },
-        {
-            // Define the configuration for `.astro` file.
-            files: ["*.astro"],
-            // Allows Astro components to be parsed.
-            parser: "astro-eslint-parser",
-            // Parse the script in `.astro` as TypeScript by adding the following configuration.
-            // It's the setting you need when using TypeScript.
-            parserOptions: {
-                parser: "@typescript-eslint/parser",
-                extraFileExtensions: [".astro"],
-            },
-            rules: {
-                // override/add rules settings here, such as:
-                // "astro/no-set-html-directive": "error"
-            },
-        },
+        "plugin:astro/recommended",
     ],
     parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module"
+        project: ["./tsconfig.json"],
     },
-    plugins: [
-        "react"
-    ],
-    rules: {
-    }
 }
