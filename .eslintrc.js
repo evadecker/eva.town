05dc699c109088309bea33759872e1c23b6b3552
Eva Decker <itsevadecker@gmail.com>
2023-08-20 13:21:13 -0400

    Init eslint

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    overrides: [
+        {
+            env: {
+                "node": true
+            },
+            files: [
+                ".eslintrc.{js,cjs}"
+            ],
+            parserOptions: {
+                sourceType: "script"
+            }
+        },
+        {
+            // Define the configuration for `.astro` file.
+            files: ["*.astro"],
+            // Allows Astro components to be parsed.
+            parser: "astro-eslint-parser",
+            // Parse the script in `.astro` as TypeScript by adding the following configuration.
+            // It's the setting you need when using TypeScript.
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+                extraFileExtensions: [".astro"],
+            },
+            rules: {
+                // override/add rules settings here, such as:
+                // "astro/no-set-html-directive": "error"
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: [
+        "react"
+    ],
+    rules: {
+    }
+}
