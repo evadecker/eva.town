ea9d288543f3e87e36d9ecaee4ef36c004b1e0a1
Eva Decker <itsevadecker@gmail.com>
2023-08-23 17:11:36 -0400

    Add Prettier and format files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,11 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    extends: [
-        "standard-with-typescript",
-        "plugin:astro/recommended",
-    ],
-    parserOptions: {
-        project: ["./tsconfig.json"],
-    },
-}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard-with-typescript", "plugin:astro/recommended"],
+  parserOptions: {
+    project: ["./tsconfig.json"],
+  },
+};
