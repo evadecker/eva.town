da9779da3a7d2a1330ff1996fd5222da8003803c
Eva Decker <itsevadecker@gmail.com>
2023-08-23 20:02:50 -0400

    Comment lines cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
 });
--- src/components/DialogueTree.tsx
+++ src/components/DialogueTree.tsx
@@ -1,61 +1,53 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
-import YarnBound, { OptionsResult, TextResult } from 'yarn-bound'
-import { DialogueNode } from './DialogueNode'
-import useForceUpdate from 'use-force-update'
-
-// Not working
-// enum Speed {
-//   'instant' = 0,
-//   'slow' = 1,
-//   'medium' = 0.06,
-//   'fast' = 0.01
-// }
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import YarnBound, { OptionsResult, TextResult } from "yarn-bound";
+import { DialogueNode } from "./DialogueNode";
+import useForceUpdate from "use-force-update";
 
 interface DialogueTreeProps {
   /**
    * String of dialogue following the YarnSpinner 2.0 spec
    * https://docs.yarnspinner.dev
    */
-  dialogue: string
+  dialogue: string;
 }
 
 const DialogueTree = ({ dialogue }: DialogueTreeProps) => {
-  const [runner, setRunner] = useState<YarnBound | null>(null)
+  const runnerRef = useRef(
+    new YarnBound({
+      dialogue,
+    })
+  );
 
-  const runnerRef = useRef(new YarnBound({
-    dialogue
-  }))
+  const forceUpdate = useForceUpdate();
 
-  useEffect(() => {
-    if (dialogue === '') return
+  const advance = useCallback(
+    (optionIndex: number | undefined) => {
+      if (runnerRef?.current?.currentResult?.isDialogueEnd) {
+        // Do things
+        // setHasDialogueEnded(true)
+        // onDialogueEnd()
+      }
+      runnerRef.current.advance(optionIndex);
+      forceUpdate();
+    },
+    [runnerRef.current]
+  );
 
-    try {
-      setRunner(new YarnBound({
-        dialogue,
-        combineTextAndOptionsResults: true
-      }))
-    } catch (err) {
-      console.error(err)
-    }
-  }, [dialogue])
-
-  const forceUpdate = useForceUpdate()
-
-  const advance = useCallback((optionIndex: number | undefined) => {
-    if (runnerRef?.current?.currentResult?.isDialogueEnd) {
-      // Do things
-      // setHasDialogueEnded(true)
-      // onDialogueEnd()
-    }
-    runnerRef.current.advance(optionIndex)
-    forceUpdate()
-  }, [runnerRef.current])
+  const displayDialogue =
+    runnerRef?.current?.currentResult !== undefined &&
+    (runnerRef?.current?.currentResult instanceof TextResult ||
+      runnerRef?.current?.currentResult instanceof OptionsResult);
 
   return (
     <div>
-      {(runner?.currentResult instanceof TextResult || runner?.currentResult instanceof OptionsResult) && <DialogueNode node={runnerRef?.current?.currentResult} advance={advance} />}
+      {displayDialogue && (
+        <DialogueNode
+          node={runnerRef?.current?.currentResult}
+          advance={advance}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default DialogueTree
+export default DialogueTree;
