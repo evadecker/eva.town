f294a6ccb535b626bd51bbf8cfa54cdeda4ad8f0
Eva Decker <itsevadecker@gmail.com>
2023-09-01 12:00:53 -0400

    Sam images

--- src/components/header/Samwise.tsx
+++ src/components/header/Samwise.tsx
@@ -1,10 +1,54 @@
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useState } from "react";
+import styles from "./header.module.css";
+import { nanoid } from "nanoid";
+
+interface SamSticker {
+  id: string;
+  src: string;
+  alt: string;
+  isVisible: boolean;
+  x: number;
+  y: number;
+}
 
 export const Samwise = () => {
+  const [sams, setSams] = useState<SamSticker[]>([]);
+
+  const handleClick = () => {
+    const getNewSamSticker = (): SamSticker => {
+      const x = Math.floor(Math.random() * document.body.clientWidth - 200);
+      const y = Math.floor(Math.random() * document.body.clientHeight - 400);
+      return {
+        id: nanoid(),
+        src: `sam-${Math.floor(Math.random() * 16 + 1)}.png`,
+        alt: "Samwise",
+        isVisible: true,
+        x: x,
+        y: y,
+      };
+    };
+
+    setSams((prev) => [...prev, getNewSamSticker()]);
+  };
+
   return (
-    <motion.div drag whileDrag={{ scale: 1.2 }} dragMomentum={false}>
-      One Samwise
-    </motion.div>
+    <>
+      {sams.map(({ id, src, alt, x, y }) => (
+        <motion.img
+          key={id}
+          className={styles.samSticker}
+          src={`/images/samwise/${src}`}
+          alt={alt}
+          initial={{ opacity: 0, x: x, y: y, scale: 2 }}
+          animate={{ opacity: 1, scale: 1 }}
+          drag
+          dragMomentum={false}
+        />
+      ))}
+      <button className={styles.samButton} onClick={handleClick}>
+        Samwise
+      </button>
+    </>
   );
 };
