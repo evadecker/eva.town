54d7662467a6f09b0089094c712339b19e323f8f
Eva Decker <itsevadecker@gmail.com>
2023-09-01 23:27:19 -0400

    Component reorganization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
--- src/components/Samwise.tsx
+++ src/components/Samwise.tsx
@@ -1,10 +1,12 @@
 import { motion } from "framer-motion";
 import React from "react";
 
-export const Samwise = () => {
+const Samwise = () => {
   return (
     <motion.div drag whileDrag={{ scale: 1.2 }} dragMomentum={false}>
       One Samwise
     </motion.div>
   );
 };
+
+export default Samwise;
