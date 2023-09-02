173002cf6fb27c15613d6a621f64e00de2f84a63
Eva Decker <itsevadecker@gmail.com>
2023-09-01 22:25:51 -0400

    Make new content collection for projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
+  redirects: {
+    "/work": "/work/cityblock",
+  },
 });
