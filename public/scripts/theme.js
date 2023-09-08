75b3c6f7bf69b6dc535713eacb4e7594db56d58c
Eva Decker <itsevadecker@gmail.com>
2023-09-08 01:55:04 -0400

    Add dark mode toggle

---
+++ public/scripts/theme.js
@@ -0,0 +1,48 @@
+function getInitialTheme() {
+  // Check local storage for user preference
+  const preferredTheme = localStorage.getItem("theme");
+  if (preferredTheme == "dark") {
+    return "dark";
+  } else if (preferredTheme == "light") {
+    return "light";
+  }
+
+  // Check media query for OS-level preference
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+  if (prefersDarkScheme.matches) {
+    return "dark";
+  }
+
+  // Else default to light
+  return "light";
+}
+
+function enableDarkMode() {
+  document.body.dataset.theme = "dark";
+  localStorage.setItem("theme", "dark");
+}
+
+function enableLightMode() {
+  document.body.dataset.theme = "light";
+  localStorage.setItem("theme", "light");
+}
+
+function setInitialTheme() {
+  const initialTheme = getInitialTheme();
+  if (initialTheme == "dark") {
+    enableDarkMode();
+  } else if (initialTheme == "light") {
+    enableLightMode();
+  }
+}
+
+function toggleTheme() {
+  const currentTheme = document.body.dataset.theme;
+  if (currentTheme == "dark") {
+    enableLightMode();
+  } else if (currentTheme == "light") {
+    enableDarkMode();
+  }
+}
+
+setInitialTheme();
