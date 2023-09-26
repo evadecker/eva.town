ac09fcf10c12272fa1f53e22aeea89d27e964f8e
Eva Decker <itsevadecker@gmail.com>
2023-09-26 12:25:04 -0400

    Remove individual component index files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
-export { DialogueTree } from "./DialogueTree";
-export { Footer } from "./Footer";
-export { Header } from "./Header";
-export { Icon } from "./Icon";
-export { SamStickers } from "./SamStickers";
-export { Synth } from "./Synth";
+import { DialogueTree } from "./DialogueTree/DialogueTree";
+import Footer from "./Footer/Footer.astro";
+import Header from "./Header/Header.astro";
+import { Icon } from "./Icon/Icon";
+import { SamStickers } from "./SamStickers/SamStickers";
+import { Synth } from "./Synth/Synth";
+
+export { DialogueTree, Footer, Header, Icon, SamStickers, Synth };
