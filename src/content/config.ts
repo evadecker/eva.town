f3f50e047f8523d057dbd249f6d824179d904f6a
Eva Decker <itsevadecker@gmail.com>
2023-10-05 17:43:27 -0400

    Reorganize content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,26 @@
 import { defineCollection, z } from "astro:content";
 
+const pagesCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    datePublished: z.date(),
+    dateModified: z.date().optional(),
+  }),
+});
+
 const notesCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    datePublished: z.string().datetime({ precision: 0 }),
+    datePublished: z.date(),
     dateModified: z.date().optional(),
   }),
 });
 
 export const collections = {
+  pages: pagesCollection,
   notes: notesCollection,
 };
--- src/content/notes/hello.md
+++ src/content/notes/hello.md
@@ -5,3 +5,4 @@ datePublished: 2023-10-05 16:33:31-04:00
 ---
 
 It's a new note.
+Testing!
