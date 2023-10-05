4b1e8b64071f95a023492e7fa0f9996d573d7b4b
Eva Decker <itsevadecker@gmail.com>
2023-10-05 16:41:38 -0400

    Add notes, remove work

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,20 +10,6 @@ const notesCollection = defineCollection({
   }),
 });
 
-const workCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    startYear: z.number(),
-    endYear: z.number(),
-    url: z.string().url(),
-    challenge: z.string(),
-    role: z.string(),
-  }),
-});
-
 export const collections = {
   notes: notesCollection,
-  work: workCollection,
 };
---
+++ src/content/notes/hello.md
@@ -0,0 +1,7 @@
+---
+title: Hello
+description: Hello world
+datePublished: 2023-10-05 16:33:31-04:00
+---
+
+It's a new note.
