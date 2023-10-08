50d80174ad7a065278f344a2bf7451835440d270
Eva Decker <itsevadecker@gmail.com>
2023-10-08 01:33:00 -0400

    Fix schema, install sharp

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,14 +15,15 @@ const pagesCollection = defineCollection({
 
 const notesCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    datePublished: z.date(),
-    dateModified: z.date().optional(),
-    img: z.string(),
-    imgAlt: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      datePublished: z.date(),
+      dateModified: z.date().optional(),
+      img: image().optional(),
+      imgAlt: z.string().optional(),
+    }),
 });
 
 export const collections = {
