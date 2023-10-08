85bdc1560e970845866f59613fb77768b1d733ef
Eva Decker <itsevadecker@gmail.com>
2023-10-08 01:25:30 -0400

    Add about image, use Astro Image

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,12 +2,15 @@ import { defineCollection, z } from "astro:content";
 
 const pagesCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    datePublished: z.date(),
-    dateModified: z.date().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      datePublished: z.date(),
+      dateModified: z.date().optional(),
+      img: image().optional(),
+      imgAlt: z.string().optional(),
+    }),
 });
 
 const notesCollection = defineCollection({
