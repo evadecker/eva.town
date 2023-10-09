b629400e4356219c16e2c13fe8beac22d3fb4190
Eva Decker <itsevadecker@gmail.com>
2023-10-09 17:25:48 -0400

    feat: Improve dithering on small viewports, add header image to colophon (#179)

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,7 +8,7 @@ const pagesCollection = defineCollection({
       description: z.string(),
       datePublished: z.date(),
       dateModified: z.date().optional(),
-      img: image().optional(),
+      img: image().array().optional(),
       imgAlt: z.string().optional(),
     }),
 });
@@ -21,7 +21,7 @@ const notesCollection = defineCollection({
       description: z.string(),
       datePublished: z.date(),
       dateModified: z.date().optional(),
-      img: image().optional(),
+      img: image().array().optional(),
       imgAlt: z.string().optional(),
     }),
 });
