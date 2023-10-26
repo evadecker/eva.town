9ccbfd7db030e9010295bb806c85cd11570d6a10
Eva Decker <itsevadecker@gmail.com>
2023-10-26 19:28:30 -0400

    Projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,34 +1,45 @@
 import { defineCollection, z } from "astro:content";
 
-const pagesCollection = defineCollection({
-  type: "content",
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      datePublished: z.date(),
-      dateModified: z.date().optional(),
-      img: image().array().optional(),
-      imgAlt: z.string().optional(),
-      ogImage: image().optional(),
-    }),
-});
-
-const notesCollection = defineCollection({
-  type: "content",
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      datePublished: z.date(),
-      dateModified: z.date().optional(),
-      img: image().array().optional(),
-      imgAlt: z.string().optional(),
-      ogImage: image().optional(),
-    }),
-});
-
 export const collections = {
-  pages: pagesCollection,
-  notes: notesCollection,
+  notes: defineCollection({
+    type: "content",
+    schema: ({ image }) =>
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        datePublished: z.date(),
+        dateModified: z.date().optional(),
+        img: image().array().optional(),
+        imgAlt: z.string().optional(),
+        ogImage: image().optional(),
+      }),
+  }),
+
+  pages: defineCollection({
+    type: "content",
+    schema: ({ image }) =>
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        datePublished: z.date(),
+        dateModified: z.date().optional(),
+        img: image().array().optional(),
+        imgAlt: z.string().optional(),
+        ogImage: image().optional(),
+      }),
+  }),
+
+  projects: defineCollection({
+    type: "data",
+    schema: ({ image }) =>
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        datePublished: z.date(),
+        img: image().array().optional(),
+        imgAlt: z.string().optional(),
+        url: z.string(),
+        repo: z.string().optional(), // Github repo, e.g. evadecker/america-my-face
+      }),
+  }),
 };
