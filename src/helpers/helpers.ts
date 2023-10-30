f0836511cf43cabbb81016dcdc180cf54c8888ad
Eva Decker <itsevadecker@gmail.com>
2023-10-29 20:09:59 -0400

    Add Andrea Lawlor quote

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -14,3 +14,13 @@ export const isValidEmail = (email: string) => {
   const emailRegex = /[a-z0-9]+@[a-z]+\.[a-z]{2,8}/;
   return email.match(emailRegex);
 };
+
+export const smartquotes = (str: string) => {
+  return str
+    .replace(/(^|[-\u2014\s(\["])'/g, "$1\u2018") // opening singles
+    .replace(/'/g, "\u2019") // closing singles & apostrophes
+    .replace(/(^|[-\u2014/\[(\u2018\s])"/g, "$1\u201c") // opening doubles
+    .replace(/"/g, "\u201d") // closing doubles
+    .replace(/--/g, "\u2014") // em-dashes
+    .replace(/\.\.\./g, "\u2026"); // ellipses
+};
