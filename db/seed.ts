7b41e554266005d04c8546b719cc6a9bc7f55916
Eva Decker <itsevadecker@gmail.com>
2024-05-18 10:13:01 -0400

    Seed data

--- db/seed.ts
+++ db/seed.ts
@@ -3,29 +3,31 @@ import { db, Guestbook } from "astro:db";
 export default async function seed() {
   await db.insert(Guestbook).values([
     {
-      author: "John Doe",
+      author: "jimmy",
       content: "You are so cool!",
       timestamp: new Date("2021-01-01T12:00:00Z"),
     },
     {
-      author: "John Doe, The Second",
-      content: "I agree with my dad!",
+      author: "gerald ford (she/her)",
+      content: "what! what. hello, this site is so cute.",
       timestamp: new Date("2021-01-01T12:01:00Z"),
     },
     {
       author: "boomy",
-      content: "  we did this",
+      content: "I love you 🩷",
       timestamp: new Date("2023-03-15T12:01:00Z"),
     },
     {
-      author: "fulllength",
+      author: "180 characters",
       content:
         "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890",
       timestamp: new Date("2024-04-12T12:01:00Z"),
     },
     {
       author: "text emotes with space",
-      content: "          ʕ•́ᴥ•̀ʔっ♡          ",
+      content: `
+
+           ʕ•́ᴥ•̀ʔっ♡`,
     },
     {
       author: "With URL",
