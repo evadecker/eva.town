d327471f5af95246c9190bb7e24434bca6e3a96a
Eva Decker <itsevadecker@gmail.com>
2024-06-14 21:02:56 -0400

    feat: Add RSS! (#441)

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,30 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const posts = await getCollection("posts");
+
+  return await rss({
+    title: "Eva Town",
+    description: "Dispatches from the world wide web.",
+    site: context.site,
+    trailingSlash: false,
+    stylesheet: "/rss/pretty-feed-v3.xsl",
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.datePublished,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat([
+          "img",
+          "figure",
+          "figcaption",
+        ]),
+      }),
+    })),
+  });
+}
