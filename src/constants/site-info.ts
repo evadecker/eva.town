9f25d6fce7b18312b805661036c1028a14d0c716
Ky Decker <itsevadecker@gmail.com>
2026-09-04 23:30:56 -0400

    Style back link, update footer

--- src/constants/site-info.ts
+++ src/constants/site-info.ts
@@ -11,8 +11,8 @@ export const siteInfo = {
   email: "hi@ky.fyi",
   socials: [
     { name: "Are.na", href: "https://www.are.na/ky-decker/" },
-    { name: "Bluesky", href: "https://bsky.app/profile/ky.fyi" },
     { name: "GitHub", href: "https://github.com/kydecker" },
     { name: "LinkedIn", href: "https://linkedin.com/in/kyfyi" },
+    { name: "RSS", href: "/rss.xml" },
   ] satisfies SocialLink[],
 } as const;
